9c330d54a1d06ba4716d184750bc651ecc4a082e
Harshavardhan <reddyvardhan7890@gmail.com>
2026-06-07 20:31:01 +0530

    New file

--- roboshop.sh
+++ roboshop.sh
@@ -5,17 +5,29 @@ AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z014115838BJ0WT42DT0W"
 DOMAIN_NAME="daws88sonline.online"
 
-for instance in $@
+for instance in "$@"
 do
     INSTANCE_ID=$( aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type "t3.micro" \
-    --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text )
+        --image-id "$AMI_ID" \
+        --instance-type "t3.micro" \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+        --query 'Instances[0].InstanceId' \
+        --output text )
 
-    if [ $instance == "frontend" ]; then
+    rc=$?
+
+    if [ $rc -ne 0 ]; then
+        echo "Failed to launch instance: $instance"
+        exit 1
+    else
+        echo "InstanceId=$INSTANCE_ID launched successfully"
+    fi
+
+  echo "Name=$instance launched successfully"
+  
+
+if [ $instance == "frontend" ]; then
         IP=$(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
@@ -33,31 +45,29 @@ do
         RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
     fi
 
-    echo "IP Address: $IP"
+    echo "IP address of $instance is $IP"
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
-        "Comment": "Updating record",
+        "Comment": "Updating record for '"$instance"'",
         "Changes": [
             {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "'$RECORD_NAME'",
-                "Type": "A",
-                "TTL": 1,
-                "ResourceRecords": [
-                {
-                    "Value": "'$IP'"
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                        {
+                            "Value": "'"$IP"'"
+                        }
+                    ]
                 }
-                ]
-            }
             }
         ]
-    }
-    '
+    }'
 
-    echo "record updated for $instance"
-
+    echo "DNS record for $instance updated successfully"
 done
