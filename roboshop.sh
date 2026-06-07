9f9a9de28d024cef3f8ab818229e7290ef81d84d
Harshavardhan <reddyvardhan7890@gmail.com>
2026-06-07 13:45:27 +0530

    new file

--- roboshop.sh
+++ roboshop.sh
@@ -1,21 +1,26 @@
+#!/bin/bash
+
 SG_ID="sg-057bbd40346b0a45f"
 AMI_ID="ami-0220d79f3f480ecf5"
-$rc  = $?
 
-for instance $@
-do 
-  instance_id=$(aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type t3.micro \
-    --security-group-ids $SG_ID \
-    --tag-specifications 'ResourceType=instance,Tags=[{Key=Harsha,Value=vardhan}]' \
-    --query 'Instances[0].PrivateIpAddress' \
-    --output text)
+for instance in "$@"
+do
+    instance_id=$(aws ec2 run-instances \
+        --image-id "$AMI_ID" \
+        --instance-type t3.micro \
+        --security-group-ids "$SG_ID" \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Harsha,Value=vardhan}]" \
+        --query 'Instances[0].InstanceId' \
+        --query 'Instances[0].PrivateIpAddress' \
+        --output text)
 
+    rc=$?
+
+    if [ $rc -ne 0 ]; then
+        echo "Failed to launch instance: $instance"
+        exit 1
+    else
+        echo "InstanceId=$instance_id launched successfully"
+    fi
 done
-if [ $rc -ne 0 ]; then
-  echo "Failed to launch instances"
-  exit 1
-else
-  echo "InstanceId=$instance_id launched successfully"
-fi
+  echo "private ip address =$instance_id lanched successfully"
